03be5fbd90bd84b59381faedddc7008f82f4f5e8
HSg666 <1303574731@qq.com>
2025-11-18 16:58:35 +0800

    修改vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+
 // @ts-expect-error: No TypeScript declarations for this package
 import postcsspxtoviewport from 'postcss-px-to-viewport'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  base: './',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
